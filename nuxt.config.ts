308f31c8040a78ee1910d4e89c7554610d222d21
yonghang.fu <1228785272@qq.com>
2025-05-12 21:44:28 +0800

    chore: 在nuxt.config.ts中添加devServer配置
    在nuxt.config.ts中添加了devServer配置，设置host为'0.0.0.0'，以便在开发环境中允许外部访问

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  devServer: {
+    host: '0.0.0.0'
+  },
+
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
